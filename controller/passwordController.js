8c04bdc97e4b95dd3d52d116a8058b48bfa4275f
mirjaj@ <mirjajajijmilon@gmail.com>
2026-01-18 23:46:07 +0530

    Change render ip for email serves

--- controller/passwordController.js
+++ controller/passwordController.js
@@ -13,7 +13,11 @@ const getTransporter = () => {
             host: "smtp.gmail.com",
             port: 587,
             secure: false,
-            pool: true, // Enable connection pooling
+            pool: true,
+            family: 4,
+            connectionTimeout: 10000,
+            greetingTimeout: 5000,
+            socketTimeout: 10000,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASSWORD,
